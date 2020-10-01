4bf9c6ab5aa1fde4890c0d9ed36bc7bc8b2609bb
Stefan Todorov <stev56@abv.bg>
2020-10-01 22:51:08 +0300

    init project
    init laravel project for the new repo

---
+++ database/migrations/2020_10_01_184724_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('todo_list_id')->constrained()->onDelete('cascade');
+            $table->dateTime('deadline')->nullable();
+            $table->boolean('disabled')->default(false);
+            $table->boolean('ready')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
